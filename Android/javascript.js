4b5ca89ccc1630165a34010185a36e34bd51e755
Josh Unwin <joshunwin@gmail.com>
2018-02-11 14:43:24 +0000

    Renamed gitignore, added initial projects

---
+++ Android/javascript.js
@@ -0,0 +1,11 @@
+div {margin: o; padding: 0;}
+div div {background: #A4CA39; position: relative;}
+
+.android{
+	height: 404px; width: 334px;
+	margin: 100px auto;
+
+}
+.head{
+	width: 220px; height: 100px;
+	top: 32px;
